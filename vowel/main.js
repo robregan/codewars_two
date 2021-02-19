75b8933bb0dd4f2427f9950012d0202bc7189d1f
rob <robert_regan86@yahoo.com>
2021-02-19 17:30:32 -0500

    count the vowels

--- vowel/main.js
+++ vowel/main.js
@@ -1,13 +1,17 @@
 // Return the number (count) of vowels in the given string.
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 // The input string will only consist of lower case letters and/or spaces.
-
-function getCount(str) {
-  const vowelsCount = 0
-  for(let i = 0; i < str.length; i++){
-    if(str.charAt(i)=="a" || str.charAt(i)=="e" || str.charAt(i)=="i" || str.charAt(i)=="o" ||str.charAt(i)=="u"){
-      vowelsCount++
-    }
-   }
-  return vowelsCount
+//
+// function getCount(str) {
+//   const vowelsCount = 0
+//   for(let i = 0; i < str.length; i++){
+//     if(str.charAt(i)=="a" || str.charAt(i)=="e" || str.charAt(i)=="i" || str.charAt(i)=="o" ||str.charAt(i)=="u"){
+//       vowelsCount++
+//     }
+//    }
+//   return vowelsCount
+//   }
+  function abbrevName(name) {
+    const[first, last] = name.toUpperCase().split(' ');
+    return `${first[0]}.${last[0]}`
   }
