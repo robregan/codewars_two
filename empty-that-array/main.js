fdac8038d4b94b9fea3ab9314ccf40b88f0003fb
rob <robert_regan86@yahoo.com>
2021-03-20 19:51:01 -0400

    empty that array! - 7kyu

---
+++ empty-that-array/main.js
@@ -0,0 +1,11 @@
+// Today's task is to write a function empty(), which take an array as argument. This function should return the same array, but empty.
+// To make it even more fun, you are not allowed to use any kind of loop (nor recursion)
+// Good luck!
+
+function empty(array) {
+  array.length = 0
+  return array
+}
+
+// could also do ...  return array.splice(0)
+
