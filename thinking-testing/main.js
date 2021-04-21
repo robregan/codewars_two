1622240a092f0012ea332a9fdb7f7bbf0c9f1a5c
rob <robert_regan86@yahoo.com>
2021-04-21 08:16:48 -0600

    thinking and testing - 7kyu

---
+++ thinking-testing/main.js
@@ -0,0 +1,20 @@
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+// //a+b?
+// Test.assertSimilar(testit(0,1), 1, "")
+// Test.assertSimilar(testit(1,2), 3, "")
+// Test.assertSimilar(testit(10,20), 30, "")
+// //a*b?
+// Test.assertSimilar(testit(1,1), 1, "")
+// Test.assertSimilar(testit(1,3), 3, "")
+// //try "Attempt" find more...
+
+function testit(a,b){
+    return a | b
+  }
