cc237ca88bf33ef42f0da87fafbdda997f685633
rob <robert_regan86@yahoo.com>
2021-05-04 00:43:05 -0400

    sum arrays - 8kyu

---
+++ sum-arrays/main.js
@@ -0,0 +1,31 @@
+// Description:
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty, return 0
+
+function sum(numbers) {
+    return numbers.reduce((a, b) => a + b, 0);
+  }
+
+// without reduce
+sum = function (numbers) {
+    "use strict";
+    var total = 0;
+    for(var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    return total;
+  };
