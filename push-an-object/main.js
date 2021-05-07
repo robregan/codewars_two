da2a60ad574dbf6e2753775bad9ab9085063a9e2
rob <robert_regan86@yahoo.com>
2021-05-07 09:57:04 -0400

    push a hash/object into array - 8kyu

---
+++ push-an-object/main.js
@@ -0,0 +1,7 @@
+// You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+// BUGSARRAYS
+
+const items = []
+items.push({a: "b", c: "d"})
+console.log(items)
