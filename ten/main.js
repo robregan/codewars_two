098e348adde1aed14be7890d3a9cdcb6ea10492a
rob <robert_regan86@yahoo.com>
2021-06-02 08:05:16 -0400

    take a ten minute walk - 6kyu

---
+++ ten/main.js
@@ -0,0 +1,25 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+function isValidWalk(walk) {
+  let directions = {
+    n: 0,
+    s: 0,
+    w: 0,
+    e: 0
+  };
+
+  walk.forEach(el => (directions[el] += 1));
+
+  return walk.length == 10 && directions.n === directions.s && directions.w === directions.e;
+}
+
+// 
+function isValidWalk(walk) {
+    const north = walk.filter(item => { return item === "n" }).length;
+    const south = walk.filter(item => { return item === "s" }).length;
+    const east = walk.filter(item => { return item === "e" }).length;
+    const west = walk.filter(item => { return item === "w" }).length;
+    
+    return walk.length === 10 && north === south && east === west;
+  }
+  
