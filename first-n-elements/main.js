9b93799c448c53503bca169fd341576b7d438ccf
rob <robert_regan86@yahoo.com>
2021-06-15 13:11:13 -0400

    enumerable magic #25 - take the first n elements - 8kyu

---
+++ first-n-elements/main.js
@@ -0,0 +1,4 @@
+// Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
+
+const take = (array, n) => array.slice(0, n)
+
