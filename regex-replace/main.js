4eae636882d6460895e1526bbee5614647b13441
rob <robert_regan86@yahoo.com>
2021-06-16 15:34:38 -0400

    regex count lowercase letters - 8kyu

---
+++ regex-replace/main.js
@@ -0,0 +1,24 @@
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// lowercaseCount("abc"); ===> 3
+
+// lowercaseCount("abcABC123"); ===> 3
+
+// lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+// lowercaseCount(""); ===> 0;
+
+// lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+// lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+function lowercaseCount(str){
+    return str.length - str.replace(/[a-z]/g, '').length;
+}
+
+// with match
+function lowercaseCount(str){
+    return (str.match( /[a-z]/g) || [] ).length
+}
+// || sets the default value, as match returns null
