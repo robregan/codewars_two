3eeb76fe897a397e9c981f239dc0d260dfaeb498
rob <robert_regan86@yahoo.com>
2021-07-05 10:34:32 -0400

    Square(n) Sum - 8kyu

---
+++ square-sum/main.js
@@ -0,0 +1,12 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers){
+    return numbers.reduce((sum, current) => sum + Math.pow(current,2),0)
+    }
+
+// es6 stylee
+
+const squareSum = n => n.reduce((a,b)=>a+b**2, 0)
+
