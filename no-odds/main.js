055ca8210d090dcb1a67732ce84844b3015bd9cc
rob <robert_regan86@yahoo.com>
2021-07-09 15:50:24 -0400

    no oddities here - 7kyu

---
+++ no-odds/main.js
@@ -0,0 +1,6 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+const noOdds = val => val.filter(el=>el%2===0)
+
