89adc1ed76a135e93a7b7edb419a8cf25dfc4f33
rob <robert_regan86@yahoo.com>
2021-08-26 21:02:53 -0400

    standup 8/26 - hashmap count

---
+++ standup-8-26/main.js
@@ -0,0 +1,37 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+// https://leetcode.com/problems/jewels-and-stones/
+
+
+function findJewels(jewels, stones){
+    const map = {},
+          count = 0
+  
+    for(const jewel of jewels){
+      map[jewel] = true
+    }
+    for(const stone of stones){
+      if(map[stone]){
+        count++
+      }
+    }
+    return count
+  }
