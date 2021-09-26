76a80c9049940394692700ae34c9fa3119a10857
rob <robert_regan86@yahoo.com>
2021-09-25 23:11:01 -0400

    counter of neighbor ones - 7kyu

---
+++ counter-ones-in-arr/main.js
@@ -0,0 +1,29 @@
+// Task
+// Tranform of input array of zeros and ones to array in which counts number of continuous ones. If there is none, return an empty array
+
+// Example
+// [1, 1, 1, 0, 1] -> [3,1]
+// [1, 1, 1, 1, 1] -> [5]
+// [0, 0, 0, 0, 0] -> []
+
+function onesCounter(input) {
+    return input.join('').split('0').filter(Number).map(e => e.length)
+  }
+//
+function onesCounter(input) {
+    if (!input.includes(1)) return []
+    let map=[]
+    let counter=0
+      for(let i=0; i<input.length; i++){    
+      if(input[i]===1){
+        counter++;
+         }else{  
+           map.push(counter)
+           counter=0         
+      }
+     
+    }   
+       map.push(counter)
+    return map.filter(el=>el!==0)
+  }
+ 
