51ab0293a393dda0f6a0f454fd2b195c22b6aaa6
rob <robert_regan86@yahoo.com>
2021-10-12 19:05:02 -0400

    enumerable magic true for all? - 8kyu

---
+++ true-for-all/main.js
@@ -0,0 +1,29 @@
+// Task
+// Create a method all which takes two params:
+
+// a sequence
+// a function (function pointer in C)
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+
+// Example
+// all((1, 2, 3, 4, 5), greater_than_9) -> false
+// all((1, 2, 3, 4, 5), less_than_9)    -> True
+
+function all( arr, fun ){
+    return arr.every(v=>fun(v))
+  }
+//
+function all( arr, fun ){
+    return arr.every(fun)
+  }
+// og for loopah
+
+function all(arr, fun){
+    for (let i=0; i < arr.length; i++){
+      if (!fun(arr[i])){
+        return false
+        }
+      }
+    return true
+  } 
+  
