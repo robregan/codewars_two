14224037b575d245304788d8ac24e9f5f8830309
rob <robert_regan86@yahoo.com>
2021-10-15 11:36:13 -0400

    added list of methods to beginning

--- string-methods/main.js
+++ string-methods/main.js
@@ -1,3 +1,21 @@
+// charAt
+// charCodeAt
+// concat
+// includes
+// indexOf
+// match
+// repeat
+// replace
+// search
+// slice
+// split
+// substr
+// toLowerCase
+// toUpperCase
+// trim
+
+// Give a short description of what the method does, how it works, it's time complexity (if appropriate), and give three examples of it in action!
+
 
 // charAt
 // The String object's charAt() method returns a new string consisting of the single UTF-16 code unit located at the specified offset into the string.
