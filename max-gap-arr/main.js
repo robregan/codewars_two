fae977fb9c19ba963c113b4a378914e70a9324fd
rob <robert_regan86@yahoo.com>
2021-10-18 17:57:06 -0400

    Maximum gap (array series #4) - 7kyu

---
+++ max-gap-arr/main.js
@@ -0,0 +1,55 @@
+// Task
+// Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives and negatives also zeros_
+
+// Repetition of numbers in the array/list could occur.
+
+// The Maximum Gap is computed Regardless the sign.
+
+// Input >> Output Examples
+// maxGap ({13,10,5,2,9}) ==> return (4)
+// Explanation:
+// The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+// maxGap ({-3,-27,-4,-2}) ==> return (23)
+// Explanation:
+// The Maximum Gap after sorting the array is 23 , The difference between |-4- (-27) | = 23 .
+
+// Note : Regardless the sign of negativity .
+
+// maxGap ({-7,-42,-809,-14,-12}) ==> return (767)  
+// Explanation:
+// The Maximum Gap after sorting the array is 767 , The difference between | -809- (-42) | = 767 .
+
+// Note : Regardless the sign of negativity .
+
+// maxGap ({-54,37,0,64,640,0,-15}) //return (576)
+// Explanation:
+// The Maximum Gap after sorting the array is 576 , The difference between | 64 - 640 | = 576 .
+
+// Note : Regardless the sign of negativity .
+
+function maxGap (numbers){
+    let gap = 0
+    let revSortedArr = numbers.sort((a,b)=>b-a)
+    for(let i = 0; i < revSortedArr.length; i++){
+      if(revSortedArr[i] - revSortedArr[i+1] > gap ){
+        gap = revSortedArr[i] - revSortedArr[i+1]
+      }
+    }return gap
+  }
+// with a reducah
+function maxGap(numbers) {
+    return numbers
+      .sort((a, b) => a - b)
+      .reduce((max, n, i) => {
+        let gap = numbers[i + 1] - n
+        if (max < gap) {
+          max = gap
+        }
+        return max
+      }, 0);
+  }
