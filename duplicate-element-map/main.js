c132aa2031172bb9340e848f10b8252356d7532e
rob <robert_regan86@yahoo.com>
2021-10-20 15:57:47 -0400

    duplicate encoder - 6kyu

---
+++ duplicate-element-map/main.js
@@ -0,0 +1,37 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+function duplicateEncode(word){
+    const letterArr = word.toLowerCase().split('')
+     return letterArr.map((element, ind, array ) => array.indexOf(element) == array.lastIndexOf(element) ? '(' : ')' ).join('')
+     }
+
+// 
+const duplicateEncode = word => word.toLowerCase().split('')
+                  .map((element, ind, array) => array.indexOf(element) === array.lastIndexOf(element) ? '(' : ')').join('')
+
+
+// without map
+function duplicateEncode(word){
+   
+    var unique='';
+    word = word.toLowerCase();
+    for(var i=0; i<word.length; i++){
+        if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){
+            unique += '(';
+        }
+        else{
+            unique += ')';
+        }
+    }
+    return unique;
+
+}
