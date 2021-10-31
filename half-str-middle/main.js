cbdcd61ad8e0a1eadfbea5892880b7d9246b35b9
rob <robert_regan86@yahoo.com>
2021-10-31 14:27:57 -0400

    middle me - 7kyu

---
+++ half-str-middle/main.js
@@ -0,0 +1,28 @@
+// Write a function that will take a key of X and place it in the middle of Y repeated N times.
+
+// Extra challege (not tested): You can complete this with under 70 characters without using regex. Challenge yourself to do this. It wont be best practices but it will work.
+
+// Rules:
+
+// If X cannot be placed in the middle, return X.
+
+// N will always be > 0.
+
+// Example:
+
+// X = 'A';
+
+// Y = '*';
+
+// N = 10;
+
+// Y repeated N times = '**********';
+
+// X in the middle of Y repeated N times = '*****A*****';
+
+function middleMe(N, X, Y){
+    if(N % 2) return X
+    const halfy = Y.repeat(N/2)
+    return halfy + X + halfy
+  }
+  
