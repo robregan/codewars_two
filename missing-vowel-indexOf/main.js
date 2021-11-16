da07d6a3742c31a40e385a5d1c2437df0f0fdf58
rob <robert_regan86@yahoo.com>
2021-11-15 22:09:29 -0500

    absent vowel - 7kyu

---
+++ missing-vowel-indexOf/main.js
@@ -0,0 +1,33 @@
+// Your job is to figure out the index of which vowel is missing from a given string:
+
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowels but one. Also, you won't need to worry about capitals.
+
+// Examples
+// "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+// "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
+
+function absentVowel(x){
+    const vowels = 'aeiou'
+    for(let i = 0; i < x.length; i++){
+      if(x.indexOf(vowels[i]) === -1){
+        return i
+      }
+   }
+}
+//
+const absentVowel = string =>
+  [...'aeiou'].findIndex(letter => !string.includes(letter))
+
+  // 
+  function absentVowel(x){
+    let vowels = {'a' : 0, 'e' : 1, 'i' : 2, 'o' : 3, 'u' : 4};
+//  
+    for(let i in vowels) {
+      if(!x.includes(i)) return vowels[i];
+    }
+  }
