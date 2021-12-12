3f12a0aa2b6a4f65411c6fdd87783c79d584d586
rob <robert_regan86@yahoo.com>
2021-12-11 19:53:35 -0500

    send it

--- hashmap-pairs-two-sum/main.js
+++ hashmap-pairs-two-sum/main.js
@@ -3,8 +3,8 @@
 // describe("Two Sum", () => {
 //  it("Should implement two sum", () => {
 //   assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
-//  });
-// });
+//  })
+// })
 
 // Hint:  Create an array to hold winning pairs and  create an object to store each number looped through. See if the sum - current number exists in the object, if so push current and stored value to your pairs array
 
