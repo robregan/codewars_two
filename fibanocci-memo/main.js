a22610926961b17c288f8b1830036ed17ca1930c
rob <robert_regan86@yahoo.com>
2021-12-16 18:37:42 -0500

    fib sequence using memoization

---
+++ fibanocci-memo/main.js
@@ -0,0 +1,41 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+// Example 2:
+
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+// Example 3:
+
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+// using memoization for a faster solution! 
+
+const memo = {}
+function fib(n) {
+    if(n <= 1){
+        return n
+    }else if( memo[n] ){
+        return memo[n]
+    }else{
+        let result = fib(n - 1) + fib(n - 2)
+        memo[n] = result
+        return result
+    }
+}
+
+console.log(fib(6))
