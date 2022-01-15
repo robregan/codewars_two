86fe88ec14754afe7510bf9389c45e118374b5cd
rob <robert_regan86@yahoo.com>
2022-01-15 09:10:16 -0500

    leetcode roman to integer

---
+++ roman-to-integer/main.js
@@ -0,0 +1,45 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// reduceRight! https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduceRight
+
+const romanToInt = s => {
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,     
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    const sVal = s.split('').map(s => roman[s])
+    return sVal.reduceRight((a, c, i, arr) => (c < arr[i + 1]) ? a - c : a + c)
+}
+
+//
+var romanToInt = function(s) {
+    const codex = {'I':1,'V':5,'X':10,'L':50,'C':100, 'D':500,'M':1000};
+    return s.split('').reduce((total, val, idx) => {
+      codex[val] < codex[s[idx + 1]] ? total -= codex[val] : total += codex[val];
+      return total
+    }, 0)
+  };
