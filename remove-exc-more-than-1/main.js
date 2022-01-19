51a1bdf1e6b8905f86daa90fe0eef6291a39b85c
rob <robert_regan86@yahoo.com>
2022-01-19 11:38:55 -0500

    exclamation marks series #7 - 7kyu

---
+++ remove-exc-more-than-1/main.js
@@ -0,0 +1,15 @@
+// Description:
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+
+function remove(s){
+    return s.split(' ').filter(word => word.split('!').length != 2).join(' ')
+  }
