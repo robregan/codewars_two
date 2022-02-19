e88d60f33cddcfbfdfe0c24e583d99f7fde053b5
rob <robert_regan86@yahoo.com>
2022-02-18 21:12:34 -0500

    remove the ! from the end of the str! 8kyu

---
+++ remove-from-end-str/main.js
@@ -0,0 +1,16 @@
+// Description:
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi!!"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+// Note
+// Please don't post issue about difficulty or duplicate.
+
+const remove = s => s.replace(/!$/, '');
+// $ means at the end of the string 
