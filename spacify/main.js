4d1d5e00a888bc22730b70a154eedc8c66998654
rob <robert_regan86@yahoo.com>
2022-03-24 15:49:34 -0400

    spacify - 7kyu

---
+++ spacify/main.js
@@ -0,0 +1,6 @@
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+
+const spacify = s => s.split('').join(' ')
